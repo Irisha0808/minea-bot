6191fe9aeedc551816152271b6c691dc224527b3
John Fink <johnfink2012@gmail.com>
2025-04-29 14:08:30 +0300

    Вернул puppeteer вместо puppeteer-core

--- minea_scraper_bot.js
+++ minea_scraper_bot.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer'); // обратно, как было;
 const { Telegraf, Markup } = require('telegraf');
 
 const bot = new Telegraf('8114664828:AAF2rP7DlgsisFptD3TDDB3Ng2E7L8-lGg8');
